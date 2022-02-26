7ddddbdbe66fa7535dbdd4d23cb8c8173fd746a3
yoshiioko <acardoza@pdx.edu>
2022-02-25 21:17:06 -0800

    Add: implemented new script to compile/run contract.

---
+++ scripts/run.js
@@ -0,0 +1,22 @@
+// run cmd: npx hardhat run scripts/run.js
+
+const { ethers } = require("hardhat");
+
+const main = async () => {
+  const nftContractFactory = await ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
