a8325669ea8724216dfa0ea2e43f8a0f66ab84cc
yoshiioko <acardoza@pdx.edu>
2022-02-27 10:37:06 -0800

    Add: implemented script to deploy contract to Rinkeby and mint an NFT.

---
+++ scripts/deploy.js
@@ -0,0 +1,33 @@
+// run cmd: npx hardhat run scripts/deploy.js
+
+const { ethers } = require("hardhat");
+
+const main = async () => {
+  const nftContractFactory = await ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+
+  // call the function
+  let txn = await nftContract.makeAnEpicNFT();
+  // Wait for it to be minted
+  await txn.wait();
+  console.log("Minted NFT #1");
+
+  txt = await nftContract.makeAnEpicNFT();
+  // Wait for it to be minted
+  await txn.wait();
+  console.log("Minted NFT #2");
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
