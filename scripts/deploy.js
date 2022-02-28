947cc97c525501035288572c2dfae8ae0d53fce3
yoshiioko <acardoza@pdx.edu>
2022-02-27 17:47:26 -0800

    Update: remove minting calls in deploy script.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,12 +7,6 @@ const main = async () => {
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
-
-  // call the function
-  let txn = await nftContract.makeAnEpicNFT();
-  // Wait for it to be minted
-  await txn.wait();
-  console.log("Minted NFT #1");
 };
 
 const runMain = async () => {
